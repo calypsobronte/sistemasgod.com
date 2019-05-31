6114cd10461025e8e6822e35719c98422e41db39
Lina <lina.montano@godconsulting.co>
2019-05-31 09:22:15 -0500

    Initial Commit where we created the website

---
+++ assets/js/main.js
@@ -0,0 +1,93 @@
+/*------------------------------------------------------------------
+        Company: Asesorias en Sistemas GOD S.A.S
+        Website Company: sistemasgod.com  
+        Author: Lina María Montaño Ramírez (Calypso Brontë  🦄 🔻 )
+        Website Author: calypsobronte.me
+        Creation date: 30/05/2019
+        City/Country: Medellín/Colombia
+        Project: Sitio web en Contrucción
+        Version:  v1.0.0
+        License: MIT
+        -----------------------------Modifications-------------------------
+        Last change: 31/05/2019  
+        Assigned to:  Lina María Montaño Ramírez (Calypso Brontë  🦄 🔻 )
+        -------------------------------------------------------------------
+-------------------------------------------------------------------*/ 
+(function ($) {
+    "use strict";
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+    
+    
+    /*==================================================================
+    [ Simple slide100 ]*/
+
+    $('.simpleslide100').each(function(){
+        var delay = 7000;
+        var speed = 1000;
+        var itemSlide = $(this).find('.simpleslide100-item');
+        var nowSlide = 0;
+
+        $(itemSlide).hide();
+        $(itemSlide[nowSlide]).show();
+        nowSlide++;
+        if(nowSlide >= itemSlide.length) {nowSlide = 0;}
+
+        setInterval(function(){
+            $(itemSlide).fadeOut(speed);
+            $(itemSlide[nowSlide]).fadeIn(speed);
+            nowSlide++;
+            if(nowSlide >= itemSlide.length) {nowSlide = 0;}
+        },delay);
+    });
+
+
+})(jQuery);
